6bf932779661dacbb9fbb212bed923e6062d1aa4
Esantomi <pilinnasa@gmail.com>
2021-08-17 09:25:20 +0900

    example source codes of the android programming class

---
+++ ch3_TextView_attributes2/app/src/main/java/kr/ac/knou/ch3_textview_attributes2/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.knou.ch3_textview_attributes2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
